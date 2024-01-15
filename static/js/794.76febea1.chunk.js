d6121532393fccbdafab497418ac758d84537fcb
deathfluffy <134225007+deathfluffy@users.noreply.github.com>
2024-01-15 11:27:30 +0000

    Deploying to gh-pages from @ deathfluffy/goit-react-hw-05-movies@0fa0988c7d3d93b3f0ff01c278e37d5f4c187415 🚀

---
+++ static/js/794.76febea1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{794:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var r=n(87),o=n(184);function a(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("p",{children:"Page not found"}),(0,o.jsx)(r.rU,{to:"/",children:"Go to Homepage"})]})}}}]);
+//# sourceMappingURL=794.76febea1.chunk.js.map
