2a90f216cb655984ae64a04e48732dc0c64de580
deathfluffy <mrandreu454@gmail.com>
2024-01-15 12:32:43 +0200

    ++++

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,31 @@
 import React, { Suspense, lazy } from 'react';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import { Layout } from 'components/Layout/Layout';
-import  Loader   from './Loader/Loader';
+import Loader from './Loader/Loader';
 
-
-const Home = lazy(() => import('pages/HomePage/Home'))
-const MoviesDetails = lazy(() => import('pages/MoviesDetails/MoviesDetails'))
+const NotFoundPage = lazy(() => import("pages/NotFoundPage/NotFoundPage"));
+const Home = lazy(() => import('pages/HomePage/Home'));
+const MoviesDetails = lazy(() => import('pages/MoviesDetails/MoviesDetails'));
 const Movies = lazy(() => import('pages/Movies/Movies'));
 const Cast = lazy(() => import('./Cast/Cast'));
 const Reviews = lazy(() => import('./Reviews/Reviews'));
 
-
 export const App = () => {
   return (
-  <div>
-   <Layout>
-     <Suspense fallback={<Loader />}>
-      <Routes>    
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MoviesDetails />}>
-          <Route path="/movies/:movieId/cast" element={<Cast />} />
-          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-        </Route>
-      </Routes>
-    </Suspense>
-   </Layout>
-  </div>
+    <div>
+      <Layout>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/movies/:movieId" element={<MoviesDetails />}>
+              <Route path="/movies/:movieId/cast" element={<Cast />} />
+              <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
+      </Layout>
+    </div>
   );
 };
-
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import css from './SearchBar.module.css';
 
-export default function SearchBar  ({ setSearchParams })  {
+export default function SearchBar({ setSearchParams }) {
   const [query, setQuery] = useState('');
 
-  const handleSubmit = e => {
-    e.preventDefault();
+  useEffect(() => {
+    const storedQuery = localStorage.getItem('searchQuery');
+    if (storedQuery) {
+      setQuery(storedQuery);
+      setSearchParams({ query: storedQuery });
+    }
+  }, [setSearchParams]);
 
+const handleSubmit = e => {
+  e.preventDefault();
+
+  const savedQuery = localStorage.getItem('searchQuery');
+
+  // Проверяем, если введенный запрос совпадает с сохраненным
+  if (query.trim() === savedQuery.trim()) {
+    toast.error("This didn't work.");
+  } else {
     setSearchParams({ query });
-  };
+    localStorage.setItem('searchQuery', query);
+  }
+};
+
+
 
   const handleSearchParams = ({ target: { value } }) => {
     setQuery(value);
@@ -16,17 +36,17 @@ export default function SearchBar  ({ setSearchParams })  {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input className={css.inputSearch}
+      <input
+        className={css.inputSearch}
         type="text"
         placeholder="Name movie"
         autoFocus
         value={query}
         onChange={handleSearchParams}
       />
-      <button className={css.searchButton}type="submit" disabled={!query}>
+      <button className={css.searchButton} type="submit" disabled={!query}>
         Search
       </button>
     </form>
   );
 };
-
